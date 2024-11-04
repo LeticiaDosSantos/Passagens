a569b4822977512fd0e4e063a1119b135d9c6559
Leticia dos Santos <36967995+LeticiaDosSantos@users.noreply.github.com>
2024-11-04 08:44:09 -0300

    Atualização simples

--- real-world-vue/src/views/AboutView.vue
+++ real-world-vue/src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>A site for events to better the world.</h1>
+  <div class="area">
+    <h2 class="title-page">Área do Atendente</h2>
+    <div class="row">
+      <div class="col-sm-3 atalhos">
+          <div class="card">
+          <div class="card-body card-atalho">
+              <img src="../assets/img/passes.png" alt="Imagem Redonda" class="imagem-redonda">
+              <h5 class="card-title">Meus Vouchers</h5>
+          </div>
+          </div>
+      </div>
+      <div class="col-sm-3 atalhos">
+          <div class="card">
+          <div class="card-body card-atalho">
+              <img src="../passes.png" alt="Imagem Redonda" class="imagem-redonda">
+              <h5 class="card-title">Recarga</h5>
+          </div>
+          </div>
+      </div>
+      <div class="col-sm-3 atalhos">
+          <div class="card">
+          <div class="card-body card-atalho">
+              <img src="../passes.png" alt="Imagem Redonda" class="imagem-redonda">
+              <h5 class="card-title">Viajar</h5>
+          </div>
+          </div>
+      </div>
+      <div class="col-sm-3 atalhos">
+          <div class="card">
+          <div class="card-body card-atalho">
+              <img src="../passes.png" alt="Imagem Redonda" class="imagem-redonda">
+              <h5 class="card-title">Usuários</h5>
+          </div>
+          </div>
+      </div>
+      <div class="col-sm-3 atalhos">
+          <div class="card">
+          <div class="card-body card-atalho">
+              <img src="../passes.png" alt="Imagem Redonda" class="imagem-redonda">
+              <h5 class="card-title">Recarga</h5>
+          </div>
+          </div>
+      </div>
+  </div>
+
   </div>
 </template>
